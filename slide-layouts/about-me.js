f6ade2250b5fe90aec92b66aef6468daae568b21
Beth Gilomen <beth.gilomen@favordelivery.com>
2020-02-24 18:36:17 -0600

    images

--- slide-layouts/about-me.js
+++ slide-layouts/about-me.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 export const AboutMe = () => (
-  <div style={{ textAlign: 'center' }}>
-    <img alt="" style={{ border: '10px solid #78b79c', borderRadius: '50%' }} src='https://bethgilomen.com/static/media/profile-photo.4cf5879d.png' />
-    <h3>bethgilomen.com</h3>
-    <h3>github.com/gilomen2</h3>
+  <div style={{ textAlign: 'center', display: 'grid', gridTemplateColumns: '1fr 1fr', gridColumnGap: '60px' }}>
+    <div>
+      <img alt="" style={{ border: '10px solid #78b79c', borderRadius: '50%' }} src='/static/beth.jpeg' />
+      <h3>bethgilomen.com</h3>
+      <h3>github.com/gilomen2</h3>
+    </div>
+    <div>
+      <img alt="" style={{ border: '10px solid #78b79c', borderRadius: '50%' }} src='/static/brian.png' />
+      <h3>Brian</h3>
+      <h3>@bigbunnybrian</h3>
+    </div>
   </div>
 )
